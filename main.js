e87cd86a1c8a84054b030301c5aa9f8822339c70
无声 <ivestszheng@qq.com>
2021-07-25 19:56:49 +0800

    init

---
+++ main.js
@@ -0,0 +1,6 @@
+/*
+ * @Descripttion: 
+ * @Date: 2021-07-25 18:32:58
+ * @LastEditTime: 2021-07-25 18:39:11
+ */
+import './style.css'
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/*
+ * @Descripttion: 
+ * @Date: 2021-07-25 18:37:21
+ * @LastEditTime: 2021-07-25 18:37:55
+ */
+module.exports = {
+  purge: ["./index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
